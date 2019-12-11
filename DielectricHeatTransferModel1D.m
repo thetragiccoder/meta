e740a6903b5ab73fd28215c73d455c280b89b621
thetragiccoder <58560985+thetragiccoder@users.noreply.github.com>
2019-12-11 19:52:24 +0000

    Update DielectricHeatTransferModel1D.m

--- DielectricHeatTransferModel1D.m
+++ DielectricHeatTransferModel1D.m
@@ -22,40 +22,52 @@ K = 1.6 * 10^5; % Curie constant
 g = -1;
 G = 1;
 
-E0 = 500;
-
 % Create Matrix with Initial values:
 T = ones(nx, 1) * T0;
 
 TNew = zeros(nx, 1);
 
-E = ones(nx, 1) * E0;
-
-ENew=zeros(nx, 1);
-ENew(1) = 500;
-ENew(nx) = 500;
+E = ones(nx, 1);
     
 t = 0:dt:tNew;
 
 for i = 1:length(t)
     for j = 2:nx-1
         TNew(j) = const * ((T(j + 1) - T(j)) / dx^2 + (T(j - 1) - T(j)) / dx^2); % Master equation
-         
-        if (TNew > TC)
-            ENew(j) = K / (T(j) - TC)
-        else 
-            ENew(j) = K / (2 * (TC - T(j)))
-            % ENew(j) = (4 * G / (3 * g.^2)) + (K / (8 * TNew(j) - TC))
-        end
-         
+        
         TNew(1) = const * ((T(2) - T(1)) / dx^2 + (TW - T(1)) / dx^2); %right boundary condition
         TNew(nx) = const * ((TE - T(nx))/dx^2 + (T(nx - 1) - T(nx)) / dx^2); %left boundary condition
     end
-    E = E + ENew;
+    
     T = T + TNew * dt; 
-end
+    
+for i = 1:length(t)
+    for j = 2:nx-1
  
-
+        if (T(j)>TC(j))
+            E(j) = K./(T(j)-TC(j))
+         else 
+            E(j) = K/(2*(TC(j)-T(j)))
+          %E(j) = 1./((3*g.^2)/4*G) + ((8*TNew(j)-TC(j))/K)
+         end
+      
+         if (T(1)>TC(1))
+            E(1) = K./(T(1)-TC(1))
+         else 
+             E(1) = K/(2*(TC(1)-T(1)))
+    %E(1) = 1./((3*g.^2)/4*G) + ((8*TNew(1)-TC(1))/K)
+         end
+      
+          if (T(nx)>TC(nx))
+            E(nx) = K./(T(nx)-TC(nx))
+         else 
+            E(nx) = K/(2*(TC(nx)-TNew(nx)))
+          %E(nx) = 1./((3*g.^2)/4*G) + ((8*TNew(nx)-TC(nx))/K)
+         end 
+            
+    end
+    
+    
 figure(1); % new figure
 [hAx, hLine1, hLine2] = plotyy(x, T, x, E);
 title('Dielectric Constant and Temperature of a Heated 1D Slab of BaTiO3');
@@ -64,6 +76,10 @@ ylabel(hAx(1), 'Temperature(K)'); % left y-axis
 ylabel(hAx(2), 'Dielectric Constant(\epsilon)'); % right y-axis
 %plot (x, E, 'r', 'Linewidth', 2);
 %pause(1);
+
+end
+ 
+
     
 figure(2);
 plot(T, E);
